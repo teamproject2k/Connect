4a330ac36db3a86abfa3ff2455c8ab9830ca520f
Aryan <aryankmishra@viewlift.com>
2023-11-03 18:28:30 +0530

    sendOTP

--- app/src/main/java/com/example/connect/data/repository/IAuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/example/connect/data/repository/IAuthenticationRepositoryImpl.kt
@@ -1,10 +1,88 @@
 package com.example.connect.data.repository
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.example.connect.domain.repository.IAuthenticationRepository
+import com.example.connect.presentation.ui.auth.AuthenticationActivity
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class IAuthenticationRepositoryImpl @Inject constructor(private val firebaseAuth: FirebaseAuth) : IAuthenticationRepository {
-    override suspend fun sendOtp(mobileNumber: String) {
+
+class IAuthenticationRepositoryImpl @Inject constructor(private val firebaseAuth: FirebaseAuth) :
+    IAuthenticationRepository {
+    override suspend fun sendOtp(mobileNumber: String): MutableStateFlow<FirebaseUser> {
+
+        val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:$credential")
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e)
+
+                when (e) {
+                    is FirebaseAuthInvalidCredentialsException -> {
+                        // Invalid request
+                    }
+
+                    is FirebaseTooManyRequestsException -> {
+                        // The SMS quota for the project has been exceeded
+                    }
+
+                    is FirebaseAuthMissingActivityForRecaptchaException -> {
+                        // reCAPTCHA verification attempted with null Activity
+                    }
+                }
+
+                // Show a message and update the UI
+            }
+
+            var StoredVerificationId = ""
+            var ResendToken: PhoneAuthProvider.ForceResendingToken? = null
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken,
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+                StoredVerificationId = verificationId
+                ResendToken = token
+            }
+        }
+
+        val options = PhoneAuthOptions.newBuilder(firebaseAuth)
+            .setPhoneNumber(mobileNumber) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(AuthenticationActivity()) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+
+
     }
 }
