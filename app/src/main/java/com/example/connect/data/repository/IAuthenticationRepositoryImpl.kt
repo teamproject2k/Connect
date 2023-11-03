a26a65c86a7c3488c0f1d14c20c13ab3e2cd694b
Arpit Katiyar <arpitkatiyar@countrydelight.in>
2023-11-03 18:21:45 +0530

    added code changes

--- app/src/main/java/com/example/connect/data/repository/IAuthenticationRepositoryImpl.kt
+++ app/src/main/java/com/example/connect/data/repository/IAuthenticationRepositoryImpl.kt
@@ -1,10 +1,33 @@
 package com.example.connect.data.repository
 
+import com.example.connect.common.ErrorCodes
+import com.example.connect.common.ResponseState
 import com.example.connect.domain.repository.IAuthenticationRepository
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.PhoneAuthProvider
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class IAuthenticationRepositoryImpl @Inject constructor(private val firebaseAuth: FirebaseAuth) : IAuthenticationRepository {
     override suspend fun sendOtp(mobileNumber: String) {
+    }
+
+    override suspend fun verifyOtp(verificationId: String, otp: String): ResponseState<FirebaseUser?> {
+        val credentials = PhoneAuthProvider.getCredential(verificationId, otp)
+        return try {
+            val result = firebaseAuth.signInWithCredential(credentials).await()
+            if (result.user != null) {
+                ResponseState.success(result.user)
+            } else {
+                ResponseState.error(ErrorCodes.NoUserFound)
+            }
+        } catch (exception: Exception) {
+            ResponseState.error(exception.localizedMessage ?: "")
+        }
+    }
+
+    override suspend fun getUserDetails(userId: String) {
+
     }
 }
