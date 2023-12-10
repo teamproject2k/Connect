ddf09bb266c1a4f7d92236fb7d6aa652371be015
Arpit Katiyar <arpitkatiyar@countrydelight.in>
2023-12-11 00:19:49 +0530

    updated comments

--- app/src/main/java/com/example/connect/domain/repository/IAuthenticationRepository.kt
+++ app/src/main/java/com/example/connect/domain/repository/IAuthenticationRepository.kt
@@ -6,26 +6,78 @@ import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.flow.MutableStateFlow
 
 interface IAuthenticationRepository {
+    /**
+     * Sends an OTP to the given mobile number.
+     *
+     * @param countryCode The country code of the mobile number.
+     * @param mobileNumber The mobile number.
+     * @param responseStateFlow The state flow that will be updated with the response.
+     */
     suspend fun sendOtp(
         countryCode: String,
         mobileNumber: String,
         responseStateFlow: MutableStateFlow<ResponseState<Pair<String, String>>>
     )
 
+    /**
+     * Verifies the OTP and returns the FirebaseUser if successful.
+     *
+     * @param verificationId The verification ID.
+     * @param otp The OTP.
+     * @return The response state.
+     */
     suspend fun verifyOtp(verificationId: String, otp: String): ResponseState<FirebaseUser>
 
+    /**
+     * Gets the user details from the remote database.
+     *
+     * @param userId The user ID.
+     * @return The response state.
+     */
     suspend fun getUserDetails(userId: String): ResponseState<UserDetails?>
 
 
+    /**
+     * Gets the number of users with the given name.
+     *
+     * @param name The name.
+     * @return The response state.
+     */
     suspend fun getUsersFromName(name: String): ResponseState<Int>
 
+    /**
+     * Adds the user to the remote database.
+     *
+     * @param userDetails The user details.
+     * @return The response state.
+     */
     suspend fun addUserToRemote(userDetails: UserDetails): ResponseState<Nothing>
 
+    /**
+     * Adds the user to the local database.
+     *
+     * @param userDetails The user details.
+     * @return The row ID of the inserted row.
+     */
     suspend fun addUserToLocalDb(userDetails: UserDetails): Long
 
+    /**
+     * Updates the device ID on the remote database.
+     *
+     * @param fireBaseId The Firebase ID.
+     * @param updatedDeviceId The updated device ID.
+     * @return The response state.
+     */
     suspend fun updateDeviceIdOnRemote(fireBaseId: String, updatedDeviceId: String): ResponseState<Nothing>
 
 
+    /**
+     * Updates the device ID on the local database.
+     *
+     * @param fireBaseId The Firebase ID.
+     * @param updatedDeviceId The updated device ID.
+     * @return The number of rows affected.
+     */
     suspend fun updateDeviceIdOnLocal(fireBaseId: String,updatedDeviceId: String): Int
 
 
